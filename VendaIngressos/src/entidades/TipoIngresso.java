257fe3ec382c5cf0a8a8595e6254d94071936f34
gvfirmeza <pmvieira@outlook.com>
2023-09-22 10:04:54 -0300

    criando e organizando classes

---
+++ VendaIngressos/src/entidades/TipoIngresso.java
@@ -0,0 +1,7 @@
+package entidades;
+
+public enum TipoIngresso {
+    INTEIRA, // Para ingressos inteiros
+    MEIA    // Para ingressos com desconto de meia-entrada
+}
+
---
+++ VendaIngressos/src/entidades/evento/Evento.java
@@ -0,0 +1,5 @@
+package entidades.evento;
+
+public class Evento {
+    
+}
