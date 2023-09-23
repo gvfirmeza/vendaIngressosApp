a5905f8f467b14d4814957821eb673f70eaff73f
gvfirmeza <pmvieira@outlook.com>
2023-09-22 22:15:39 -0300

    metodo cadastrar novo evento

--- VendaIngressos/src/entidades/Menu.java
+++ VendaIngressos/src/entidades/Menu.java
@@ -2,79 +2,204 @@ package entidades;
 
 import java.util.Scanner;
 
+import entidades.evento.Evento;
+import entidades.evento.Exposicao;
+import entidades.evento.Jogo;
+import entidades.evento.Show;
+import entidades.ingresso.Ingresso;
+
 public class Menu {
+
     public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
-        String cadastro;
-        String ingresso = "";
-        String nomeEvent = "";
-        String dataEvent = "";
-        String localEvent = "";
-        String maxEvent = "";
+        Scanner scanner = new Scanner(System.in);
+        Evento evento = null;
+        Ingresso ultimoIngressoVendido = null;
 
         while (true) {
-            System.out.println("Bem vindo!");
-            System.out.println("O que deseja fazer?");
-            System.out.println("Cadastrar novo evento(ce), Comprar Ingressos(ci)");
-            System.out.println("Ver as Informações do Evento(ie), Número restantes de Ingressos(ni), Ver o último ingresso comprado(ic)");
-            System.out.println("Sair(s)");
-            cadastro = leitor.next();
+            System.out.println("Menu de Opções:");
+            System.out.println("1. Cadastrar um novo evento");
+            System.out.println("2. Realizar a venda de um ingresso");
+            System.out.println("3. Exibir informações do evento");
+            System.out.println("4. Exibir o número de ingressos restantes");
+            System.out.println("5. Exibir informações do último ingresso vendido");
+            System.out.println("6. Sair");
 
-            switch (cadastro) {
-                case "ce":
-                    System.out.println("Informe o nome do evento:");
-                    nomeEvent = leitor.next();
-                    System.out.println("Informe a data do evento:");
-                    dataEvent = leitor.next();
-                    System.out.println("Qual o local desejado:");
-                    localEvent = leitor.next();
-                    System.out.println("Qual o número máximo de participantes:");
-                    maxEvent = leitor.next();
+            System.out.print("Escolha uma opção: ");
+            int escolha = scanner.nextInt();
+
+            switch (escolha) {
+                case 1:
+                    // Cadastrar um novo evento
+                    evento = cadastrarNovoEvento();
                     break;
-
-                case "ci":
-                    System.out.println("Qual ingresso necessário? Inteiro(int)/Meia");
-                    ingresso = leitor.next();
-
-                    switch (ingresso) {
-                        case "int":
-                            System.out.println("Você comprou um ingresso tipo Inteira");
-                            break;
-
-                        case "meia":
-                            System.out.println("Você comprou um ingresso tipo Meia");
-                            break;
-
-                        default:
-                            System.out.println("Ingresso inválido");
-                            break;
+                case 2:
+                    // Realizar a venda de um ingresso
+                    if (evento != null) {
+                        ultimoIngressoVendido = realizarVendaIngresso(evento);
+                    } else {
+                        System.out.println("Por favor, cadastre um evento primeiro.");
                     }
                     break;
-
-                case "ie":
-                    System.out.println("Nome do evento:" + nomeEvent);
-                    System.out.println("Data do evento:" + dataEvent);
-                    System.out.println("Local do evento:" + localEvent);
-                    System.out.println("Número máximo de participantes:" + maxEvent);
+                case 3:
+                    // Exibir informações do evento
+                    if (evento != null) {
+                        exibirInformacoesEvento(evento);
+                    } else {
+                        System.out.println("Nenhum evento cadastrado.");
+                    }
                     break;
-
-                case "ni":
-                    System.out.println("Restam 10 ingressos");
+                case 4:
+                    // Exibir o número de ingressos restantes
+                    if (evento != null) {
+                        exibirNumeroIngressosRestantes(evento);
+                    } else {
+                        System.out.println("Nenhum evento cadastrado.");
+                    }
                     break;
-
-                case "ic":
-                    System.out.println("Seu último ingresso comprado");
+                case 5:
+                    // Exibir informações do último ingresso vendido
+                    if (ultimoIngressoVendido != null) {
+                        exibirInformacoesIngresso(ultimoIngressoVendido);
+                    } else {
+                        System.out.println("Nenhum ingresso vendido ainda.");
+                    }
                     break;
-
-                case "s":
-                    System.out.println("Saindo do programa.");
+                case 6:
+                    // Sair do programa
+                    System.out.println("Saindo do programa...");
+                    scanner.close();
                     System.exit(0);
-                    break;
-
                 default:
-                    System.out.println("Opção inválida");
-                    break;
+                    System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
             }
         }
+    }
+
+    private static Evento cadastrarNovoEvento() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1. Cadastrar Exposição");
+        System.out.println("2. Cadastrar Jogo");
+        System.out.println("3. Cadastrar Show");
+        System.out.print("Escolha uma opção: ");
+        int escolha = scanner.nextInt();
+        Evento evento = null; // Variável para armazenar o evento criado
+        switch (escolha) {
+            case 1:
+                System.out.println("Informe o nome da exposição:");
+                String nomeExpo = scanner.nextLine(); 
+                nomeExpo = scanner.nextLine(); 
+
+                System.out.println("Informe a data da exposição:");
+                String dataExpo = scanner.nextLine();
+
+                System.out.println("Informe o local da exposição:");
+                String localExpo = scanner.nextLine();
+
+                System.out.println("Informe o número de ingressos inteiros disponíveis:");
+                int ingressosInteiraExpo = scanner.nextInt();
+
+                System.out.println("Informe o número de ingressos meia entrada disponíveis:");
+                int ingressosMeiaExpo = scanner.nextInt();
+
+                System.out.println("Informe a faixa etária mínima:");
+                int faixaEtariaMinimaExpo = scanner.nextInt();
+
+                System.out.println("Informe a duração em dias da exposição:");
+                int duracaoDiasExpo = scanner.nextInt();
+
+                System.out.println("Informe se há desconto social (true/false):");
+                boolean descontoSocialExpo = scanner.nextBoolean();
+
+                System.out.println("Informe o preço da exposição:");
+                double precoEventoExpo = scanner.nextDouble();
+
+                Exposicao exposicao = new Exposicao(nomeExpo, dataExpo, localExpo, ingressosInteiraExpo,
+                        ingressosMeiaExpo, faixaEtariaMinimaExpo, duracaoDiasExpo, descontoSocialExpo, precoEventoExpo);
+                evento = exposicao;
+                break;
+            case 2:
+                System.out.println("Informe o nome do jogo:");
+                String nomeJogo = scanner.nextLine();
+                nomeJogo = scanner.nextLine();
+
+                System.out.println("Informe a data do jogo:");
+                String dataJogo = scanner.nextLine();
+
+                System.out.println("Informe o local do jogo:");
+                String localJogo = scanner.nextLine();
+
+                System.out.println("Informe o número de ingressos inteiros disponíveis:");
+                int ingressosInteiraJogo = scanner.nextInt();
+
+                System.out.println("Informe o número de ingressos meia entrada disponíveis:");
+                int ingressosMeiaJogo = scanner.nextInt();
+
+                System.out.println("Informe o esporte do jogo:");
+                String esporteJogo = scanner.nextLine();
+
+                System.out.println("Informe as equipes que participarão do jogo:");
+                String equipesCompetindoJogo = scanner.nextLine();
+
+                System.out.println("Informe o preço do jogo:");
+                double precoEventoJogo = scanner.nextDouble();
+
+                System.out.println("Informe se há desconto torcedor (true/false):");
+                boolean descontoJogo = scanner.nextBoolean();
+
+                Jogo jogo = new Jogo(nomeJogo, dataJogo, localJogo, ingressosInteiraJogo, ingressosMeiaJogo,
+                        esporteJogo, equipesCompetindoJogo, precoEventoJogo, descontoJogo);
+                evento = jogo;
+                break;
+            case 3:
+                System.out.println("Informe o nome do show:");
+                String nomeShow = scanner.nextLine(); 
+                nomeShow = scanner.nextLine(); 
+
+                System.out.println("Informe a data do show:");
+                String dataShow = scanner.nextLine();
+
+                System.out.println("Informe o local do show:");
+                String localShow = scanner.nextLine();
+
+                System.out.println("Informe o número de ingressos inteiros disponíveis:");
+                int ingressosInteiraShow = scanner.nextInt();
+
+                System.out.println("Informe o número de ingressos meia entrada disponíveis:");
+                int ingressosMeiaShow = scanner.nextInt();
+
+                System.out.println("Informe o nome do artista:");
+                String nomeArtistaShow = scanner.nextLine();
+
+                System.out.println("Informe o genero de musica:");
+                String generoMusicaShow = scanner.nextLine();
+
+                System.out.println("Informe o preço do evento:");
+                double precoEventoShow = scanner.nextDouble();
+
+                System.out.println("Informe se é pista (true/false):");
+                boolean pistaShow = scanner.nextBoolean();                
+
+                Show show = new Show(nomeShow, dataShow, localShow, ingressosInteiraShow, ingressosMeiaShow, nomeArtistaShow, generoMusicaShow, pistaShow, precoEventoShow);
+                evento = show; 
+                break;
+            default:
+                System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
+            
+            scanner.close();
+        }
+        return evento;
+    }
+
+    private static Ingresso realizarVendaIngresso(Evento evento) {
+        return null; 
+    }
+
+    private static void exibirInformacoesEvento(Evento evento) {
+    }
+
+    private static void exibirNumeroIngressosRestantes(Evento evento) {
+    }
+
+    private static void exibirInformacoesIngresso(Ingresso ingresso) {
     }
 }
